a1d1b60a485594602029556e0bc1618bcbe959be
thycrescendo <dmicholn@gmail.com>
2025-12-18 09:00:57 +0100

    functional and trading dash

--- swund-frontend/pages/_app.tsx
+++ swund-frontend/pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css'; // Important: import RainbowKit CSS
+import '@rainbow-me/rainbowkit/styles.css'; 
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
-import { config } from '../lib/wagmiConfig'; // Adjust path if needed
+import { config } from '../lib/wagmiConfig'; 
 
-// Create a client (only once)
+
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
